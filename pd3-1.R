a1f4d6784e0dd7a950ef10f9e7c17e85c692c909
mwczajkowska <72414822+mwczajkowska@users.noreply.github.com>
2021-06-02 18:36:37 +0200

    Delete pd3-1.R

--- pd3-1.R
+++
@@ -1,9 +0,0 @@
-
-library("XML")
-
-result <- xmlParse("diy/Posts.xml")
-nodes <- getNodeSet(result, path = "//row")
-data <- XML:::xmlAttrsToDataFrame(nodes)
-data
-typeof(data)
-is.data.frame(data)
